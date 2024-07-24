e83fb0f2cad36f6d5b4c60560982ff2dc77180d6
abdullah <3703abdullahashraf@gmail.com>
2024-07-24 19:41:12 +0530

    sum function

--- function.c
+++ function.c
@@ -14,26 +14,43 @@
 //     printf("This is display\n");
 // }
 
+// #include<stdio.h>
+// void goodMorning();
+// void goodAfternoon();
+// void goodNight();
+
+// int main(){
+//     goodMorning();
+//     return 0;
+// }
+
+// void goodMorning(){
+//     printf("Good Morning sm0sh\n");
+//     goodAfternoon();
+// }
+
+// void goodAfternoon(){
+//     printf("Good Afternoon sm0sh\n");
+//     goodNight();
+// }
+
+// void goodNight(){
+//     printf("Good Night sm0sh\n");
+// }
+
 #include<stdio.h>
-void goodMorning();
-void goodAfternoon();
-void goodNight();
+// sum is a function which takes a and b as input and returns an integer as an output
+int sum(int a, int b); // function prototype declaration
 
 int main(){
-    goodMorning();
+    int c;
+    c = sum(2, 15); // function call
+    printf("The value of c is %d\n", c);
     return 0;
 }
 
-void goodMorning(){
-    printf("Good Morning sm0sh\n");
-    goodAfternoon();
-}
-
-void goodAfternoon(){
-    printf("Good Afternoon sm0sh\n");
-    goodNight();
-}
-
-void goodNight(){
-    printf("Good Night sm0sh\n");
+int sum (int a, int b){
+    int result;
+    result = a + b;
+    return result;
 }
