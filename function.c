941bb59a9a4b9ebe36251cf0aa58757e452d352e
abdullah <3703abdullahashraf@gmail.com>
2024-07-24 19:30:17 +0530

    function inside function

--- function.c
+++ function.c
@@ -1,15 +1,39 @@
+// #include<stdio.h>
+// void display();  //function prototype
+
+// int main(){
+//     int a;
+//     printf("Initializing display function\n");
+//     display(); //Function Call
+//     printf("Display function finished its work\n");
+//     return 0;
+// }
+
+// // Function definition
+// void display(){ 
+//     printf("This is display\n");
+// }
+
 #include<stdio.h>
-void display();  //function prototype
+void goodMorning();
+void goodAfternoon();
+void goodNight();
 
 int main(){
-    int a;
-    printf("Initializing display function\n");
-    display(); //Function Call
-    printf("Display function finished its work\n");
+    goodMorning();
     return 0;
 }
 
-// Function definition
-void display(){ 
-    printf("This is display\n");
+void goodMorning(){
+    printf("Good Morning sm0sh\n");
+    goodAfternoon();
+}
+
+void goodAfternoon(){
+    printf("Good Afternoon sm0sh\n");
+    goodNight();
+}
+
+void goodNight(){
+    printf("Good Night sm0sh\n");
 }
