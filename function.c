ea6b1d241d2d381d663565e24a5385f2957856a3
abdullah <3703abdullahashraf@gmail.com>
2024-07-25 15:27:50 +0530

    change

--- function.c
+++ function.c
@@ -39,18 +39,16 @@
 // }
 
 #include<stdio.h>
-// sum is a function which takes a and b as input and returns an integer as an output
-int sum(int a, int b); // function prototype declaration
+void change(int a);
 
 int main(){
-    int c;
-    c = sum(2, 15); // function call
-    printf("The value of c is %d\n", c);
+    int b = 344;
+    printf("The value of b before change is %d\n", b);
+    change(b);
+    printf("The value of b aftr change is %d\n", b);
     return 0;
 }
 
-int sum (int a, int b){
-    int result;
-    result = a + b;
-    return result;
+void change(int a){
+    a = 77;
 }
