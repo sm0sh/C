1bbf457cd1073aa0ebf6e2bb55315d4f78a4e9c6
Abdullashraff <3703abdullahashraf@gmail.com>
2024-07-31 14:53:19 +0530

    call by reference

--- pointer.c
+++ pointer.c
@@ -12,13 +12,40 @@
 //     return 0;
 // }
 
+// #include<stdio.h>
+// int sum (int a, int b);
+// int main(){
+//     int a=4, b=7;
+//     printf("The value of 4+7 is %d\n", sum(a, b)); 
+//     return 0;
+// }
+// int sum (int a, int b){
+//     return a + b;
+// }
+
 #include<stdio.h>
-int sum (int a, int b);
+void wrong_swap(int a, int b);
+void swap(int *a, int *b);
+
 int main(){
-    int a=4, b=7;
-    printf("The value of 4+7 is %d\n", sum(a, b)); 
+    int x=3, y=4;
+    printf("The value of x and y before swap is %d and %d\n", x, y);
+    // wrong_swap(x, y); // will not work due to call by value
+    swap(&x, &y);
+    printf("The value of x and y after swap is %d and %d\n", x,y);
     return 0;
 }
-int sum (int a, int b){
-    return a + b;
+
+void wrong_swap(int a, int b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void swap(int *a , int *b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
